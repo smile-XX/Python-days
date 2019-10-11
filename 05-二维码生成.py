587c36c18d491ce4acb666c417976df5cfed36a9
weixiao <1332360544@qq.com>
2019-10-11 22:13:51 +0800

    05二维码生成

---
+++ 05-二维码生成.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+from MyQR import myqr
+word = "I LOVE YOU"
+myqr.run(
+    words=word,
+    picture='timg.gif',
+    colorized=True,
+    save_name='qr.gif'
+)
+                                                        
